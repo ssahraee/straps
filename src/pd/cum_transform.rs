4cfd84afe300b86ff214046039f8a18cc4863ca2
Gaëtan Cassiers <gaetan.cassiers@uclouvain.be>
2021-03-24 11:47:49 +0100

    Add License

--- src/pd/cum_transform.rs
+++ src/pd/cum_transform.rs
@@ -1,3 +1,19 @@
+// STRAPS - Statistical Testing of RAndom Probing Security
+// Copyright (C) 2021 UCLouvain
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 //! Consider a probe distribution, which is a vector x of 2^n elements.
 //! We consider an index i to be a superset of an index j if (j & ~i) == 0.
 //! The cumulative transform y=T(x) of x is such that y[j] = sum_i x[i] for all i that are
